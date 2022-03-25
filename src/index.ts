7b8d4bf84b4a3d382f9c2e6aea7a765f96815bb1
Robert (Bob) Turner <r.d.turner@sheffield.ac.uk>
2022-03-25 09:29:43 +0000

    pass SHELL without switch

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ function getInputNumber(id: string, required: boolean): number | undefined {
 function getInputBoolean(id: string): Boolean {
   const input = getInput(id);
 
-  if (!['true','false'].includes(input.toLowerCase())) {
+  if (!['true', 'false'].includes(input.toLowerCase())) {
     throw `Input ${id} only accepts boolean values.  Received ${input}`;
   }
   return input.toLowerCase() === 'true'
@@ -76,38 +76,7 @@ function getTimeout(): number {
 }
 
 function getExecutable(): string {
-  if (!SHELL) {
-    return OS === 'win32' ? 'powershell' : 'bash';
-  }
-
-  let executable: string;
-  switch (SHELL) {
-    case "bash":
-    case "python":
-    case "pwsh": {
-      executable = SHELL;
-      break;
-    }
-    case "sh": {
-      if (OS === 'win32') {
-        throw new Error(`Shell ${SHELL} not allowed on OS ${OS}`);
-      }
-      executable = SHELL;
-      break;
-    }
-    case "cmd":
-    case "powershell": {
-      if (OS !== 'win32') {
-        throw new Error(`Shell ${SHELL} not allowed on OS ${OS}`);
-      }
-      executable = SHELL + ".exe";
-      break;
-    }
-    default: {
-      throw new Error(`Shell ${SHELL} not supported.  See https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-syntax-for-github-actions#using-a-specific-shell for supported shells`);
-    }
-  }
-  return executable
+  return SHELL
 }
 
 async function runRetryCmd(): Promise<void> {
@@ -132,8 +101,8 @@ async function runCmd(attempt: number) {
 
   debug(`Running command ${COMMAND} on ${OS} using shell ${executable}`)
   var child = attempt > 1 && NEW_COMMAND_ON_RETRY
-      ? exec(NEW_COMMAND_ON_RETRY, { 'shell': executable })
-      : exec(COMMAND, { 'shell': executable });
+    ? exec(NEW_COMMAND_ON_RETRY, { 'shell': executable })
+    : exec(COMMAND, { 'shell': executable });
 
   child.stdout?.on('data', (data) => {
     process.stdout.write(data);
